dc9bc220031d4574b8314a6058dcf8e6302575aa
Joaquin <joaquin@webdox.cl>
2018-09-05 10:50:11 -0300

    add better migration name

--- lib/generators/acts_as_alertable/install/install_generator.rb
+++ lib/generators/acts_as_alertable/install/install_generator.rb
@@ -3,7 +3,7 @@ module ActsAsAlertable
 	  source_root File.expand_path('../templates', __FILE__)
 
 	  def copy_initializer_file
-	  	copy_file "install.rb", "db/migrate/#{Time.now}_create_acts_as_alertable_alerts.rb"
+	  	copy_file "install.rb", "db/migrate/#{Time.now.strftime("%Y%m%d%H%M%S")}_create_acts_as_alertable_alerts.rb"
 	  end
 
 	  def migration_version
