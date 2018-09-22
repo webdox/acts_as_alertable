3ca36cc72a9f9f3a13a239ae1156693f39a14db2
Joaquin Nuñez <nunezg.ja@gmail.com>
2018-09-22 16:39:16 -0300

    fix sendeable objects

--- app/models/acts_as_alertable/alert.rb
+++ app/models/acts_as_alertable/alert.rb
@@ -135,7 +135,7 @@ module ActsAsAlertable
 
 	def send_notifications date
 		ids = trigger_dates_object[date]
-		alertable_model.where(id: ids).each do |alertable|
+		alertables.where(id: ids).each do |alertable|
 			alertable.alerteds_for(self).each do |alerted|
 				self.notify(alerted, alertable)
 			end
